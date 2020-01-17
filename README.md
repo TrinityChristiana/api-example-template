53a2021df3e49806ec79f7bab704ad0acc0b4612
Trinity Terry <trinitycterry@gmail.com>
2020-01-17 08:45:30 -0600

    Added comments

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # API-Example
 
-Simple API example playground for future testing and work with DOM.
+Simple API example playground for future testing and work with DOM. 
+WIth added comments to explain what's happening
 
+## Current View of program after button clicked
 ![screen shot](./sreenshot.png "screen shot")
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-
+// Creates variable for html element with btn as its id
 const btn = document.querySelector("#btn");
-const jokeDisplay = document.querySelector("#joke");
 
+// Creates variable for html element with container as id
+const jokeDisplay = document.querySelector("#container");
+
+// Writes Jokes to the DOM
 const populateJoke = (joke) => {
+  // appends text to element with container as id
   jokeDisplay.innerHTML += `
     <div class="card" id="${joke.id}" style="width: 18rem;">
       <div class="card-body">
@@ -10,18 +14,24 @@ const populateJoke = (joke) => {
         <p class="card-text">${joke.punchline}</p>
       </div>
     </div>
-    `
-}
+    `;
+};
 
+// fetched joke information from api
 const getJoke = () => {
+  // fetches api
   fetch('https://official-joke-api.appspot.com/jokes/programming/ten')
+    // Converts response to json file
     .then((response) => response.json())
+    // What to di with the data or in this case jokes
     .then((jokes) => {
-      jokeDisplay.innerHTML = ""
+      jokeDisplay.innerHTML = "";
+      // Maps through the array of jokes fetched from the api and runs the polulateJoke function with each joke pulled 
       jokes.map((joke) => {
-        populateJoke(joke)
-      })
+        populateJoke(joke);
+      });
     });
-}
+};
 
+// Connects getJoke function with the click of a button with the id of btn.
 btn.addEventListener('click', () => getJoke());
